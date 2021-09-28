1548c441036205ad02ac7149db17788ef381f241
Muhammad Izuddin <45591405+muhdizuddin@users.noreply.github.com>
2021-09-28 21:31:19 +0800

    Update Malay speech recognition.py

--- Malay speech recognition.py
+++ Malay speech recognition.py
@@ -1,7 +1,7 @@
 #must install = pip install malaya-speech
 import speech_recognition as sr
 import malaya_speech
-malayspeech, sr = malaya_speech.load('C:/Users/User/Desktop/master/gg.wav')
+malayspeech, sr = malaya_speech.load('Your voice directory (file must in wav format)')
 malayspeech
 small_model = malaya_speech.stt.deep_transducer(model = 'small-conformer')
 small_model.greedy_decoder([malayspeech])
